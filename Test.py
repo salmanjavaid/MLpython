47c4528607ffc5b4e182173134f819f8e916213d
salmanjavaid <javaid.salman@gmail.com>
2012-12-17 05:25:02 -0800

    MultipleFiles V.1

--- Test.py
+++ Test.py
@@ -1,22 +1,26 @@
 from BeautifulSoup import BeautifulSoup
 import multiprocessing
 import os
+a = 300001
+for k in range(1,3):
+    Index = 0
+    TrIndex = 0
+    f = open("D:\\Results\\Class9\\300000-350000\\" + str(a) + ".html", "r")
+    os.path.getsize("D:\\Results\\Class9\\300000-350000\\300001.html");
+    a = a + 1
+    soup = BeautifulSoup(f)
+    f.close()
+    tables = soup.find('table')
+    t = ""
+    for table in tables:
+        Index = Index + 1
+        if Index == 6:
+            trs = table.find("tr")
+            for i in range(1,8):
+                trs = trs.nextSibling.nextSibling
+                td = trs.find("td")
+                t = t + trs.find("td").text + "  " + td.nextSibling.nextSibling.text + " "
+    with open("D:\\Output.txt", "a") as text_file:
+        text_file.write(t + '\n')
 
-Index = 0
-TrIndex = 0
-f = open("D:\\Results\\Class9\\300000-350000\\300001.html", "r")
-os.path.getsize("D:\\Results\\Class9\\300000-350000\\300001.html");
-soup = BeautifulSoup(f)
-f.close()
-tables = soup.find('table')
-
-for table in tables:
-    Index = Index + 1
-    if Index == 6:
-        trs = table.find("tr")
-        for i in range(1,8):
-            trs = trs.nextSibling.nextSibling
-            td = trs.find("td")
-            print trs.find("td").text + "  " + td.nextSibling.nextSibling.text
             
-
